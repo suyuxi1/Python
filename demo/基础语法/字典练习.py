40a05fcab683ac853378f608ff4dfa82b3d66ec8
suyuxi1 <2296887348@qq.com>
2021-08-06 12:29:57 +0800

    python test

--- demo/基础语法/字典练习.py
+++ demo/基础语法/字典练习.py
@@ -1,7 +1,45 @@
+
 # dict = {}
 # dict['one'] = "this is one"
 # dict[2] = "this is two"
 # print (dict[2])
 
-print (complex(2,3))
+# print (complex(2,3))
+
+# i = 1
+# while i <= 5:
+#     num = int(input("输入一个数:"))
+#     print("你输入的数为：", num)
+#     i += 1
+# print("ok")
+
+
+# for item in range(-6,10,3):
+#      print(item)
+
+
+# list=[1,2,3,4]
+# it = iter(list)
+# for x in it:
+#   print(x)
+
+
+# def su(args):
+#     print(args)
+# args = "skfjhskh"
+# su(args)
+
+
+
+def printinfo(args, **vartuple):
+    # 打印传入参数
+    print("输出：")
+    print(args)
+    print(vartuple)
+
+# 调用函数
+printinfo(30,a=40,b=50,c=60,d=70)
+    
+
+
 
